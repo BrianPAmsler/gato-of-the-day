1ab23efda470e4825cdc74897f249b286d82a9a6
Brian Amsler <manof2words@yahoo.com>
2023-08-24 17:24:17 -0700

    fixed url embed

--- gato_of_the_day/bot.py
+++ gato_of_the_day/bot.py
@@ -54,7 +54,9 @@ async def ishihara(channel, client, animal):
     url = get_pic(animal)
     await run_blocking(ishi, client, url)
 
-    await channel.send(file=discord.File('output.png'), url=url)
+    embed = discord.Embed(title="", description="")
+    embed.set_image(url=url)
+    await channel.send(file=discord.File('output.png'), embed = embed)
 
 async def send_pic(channel, animal, title):
     embed = discord.Embed(title=title, description="")
